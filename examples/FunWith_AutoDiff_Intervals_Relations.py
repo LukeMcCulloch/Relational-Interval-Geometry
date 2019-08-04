e3374ec1f1dab70ea7f08d024d55d5da134e1da0
Luke McCulloch <tlukemcculloch@gmail.com>
2019-08-04 16:39:34 -0600

    initial setup of real valued scalars that will work together as vectors

--- examples/FunWith_AutoDiff_Intervals_Relations.py
+++ examples/FunWith_AutoDiff_Intervals_Relations.py
@@ -24,17 +24,31 @@ ad = automatic_differentiation.ad #
 ia = rsp.ia #interval analysis
 
 
-N=2
+num_in_vec = 2
+
+
+##
+##*****************************************
+## Make some AD (auto differentiation) numbers
 
 # of_scalars=True     : means you intend to use this number in a vector of real numbers (False is default)
-xf = ad( 2., name = 'x', N=2, dim=0, of_scalars=True)
+xf = ad( 2., name = 'x', 
+        N=num_in_vec, dim=0, of_scalars=True)
+yf = ad( 2., name = 'x', 
+        N=num_in_vec, dim=1, of_scalars=True)
 
+test = xf + yf**2
 
+print '\ntest value'
+print test.value
+print '\ntest gradient'
+print test.grad
+print '\ntest hessian'
+print test.hess
 
-
-#
-#*****************************************
-# switch to intervals 
+##
+##*****************************************
+## switch to intervals 
 x0grad = adObjectMaker.makeGradient(N,i=0)
 x0hess = adObjectMaker.makeHessian(N)
 
